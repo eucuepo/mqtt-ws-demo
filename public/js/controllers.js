96117af758f2d33330df44c9e02da1ce354914a4
Luan <147530144@qq.com>
2015-12-01 18:42:59 +0800

    add below functions on demo page: set environment register user

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,9 +1,100 @@
 'use strict';
 
+var KiiEnv = Kii;
+
 angular.module('mqttDemo.controllers',[])
 .controller('ConnectionCtrl', ['$scope',function($scope) {
 
-  $scope.message = 'Hello!';
+  $scope.init = function() {
+    console.log("init()");
+
+    $scope.KiiInfo = {
+      appID: "0ce64137",
+      appKey: "e61d8b23b67a89a944414197452d7663",
+      site: "dev-jp"
+    };
+
+    $scope.userInfo = {
+      userName:"someusername",
+      password:"somepassword",
+      userObject:{}
+    };
+
+    // $scope.isUserRegistered = false;
+    // $scope.isThingRegistered = false;
+
+    $scope.message = 'Hello!';
+
+  }
+
+  $scope.onClickSetEnvironment = function() {
+    var site;
+    if($scope.KiiInfo.site == "dev-jp") {
+      site = KiiSite["dev-jp"];
+    } else {
+      alert("please select the region");
+      return;
+    }
+
+    KiiEnv.initializeWithSite($scope.KiiInfo.appID, $scope.KiiInfo.appKey, site);
+    console.log('Kii sdk initialized')
+
+  }
+
+  $scope.onClickRegisterUser = function() {
+    console.log($scope.userInfo.userName, $scope.userInfo.password);
+
+    // Create the KiiUser object
+    var user = KiiUser.userWithUsername($scope.userInfo.userName, $scope.userInfo.password);
+
+    // Register the user, defining callbacks for when the process completes
+    user.register({
+      // Called on successful registration
+      success: function(theUser) {
+        // Print some info to the log
+        console.log("User registered!");
+        console.log(theUser);
+        console.log("user id: " + theUser._uuid);
+
+        $scope.userInfo.userObject = theUser;
+
+      },
+      // Called on a failed registration
+      failure: function(theUser, errorString) {
+        // Print some info to the log
+        console.log("Error registering: " + errorString);
+      }
+    });
+  }
+
+
+  function sendHttpRequest(method, url, headers, data, callbacks) {
+
+    var request = new XMLHttpRequest();
+
+    request.open(method, url, true);
+    request.onreadystatechange = function() {
+      if(request.readystate == 4 && request.status == 200) {
+        console.log("onComplete");
+        callbacks.onComplete(request.responseText);
+      } else {
+        console.log("onError");
+        callbacks.onError();
+      }
+    };
+
+    for (key in headers) {
+      value = headers[key];
+      request.setRequestHeader(key, value);
+    }
+
+    if(method !== "GET" && method !== "DELETE") {
+      request.send(data);
+    } else {
+      request.send();
+    }
+  }
+
 
 }])
 .controller('TestCtrl', ['$scope','mqttClient',function($scope,mqttClient) {
