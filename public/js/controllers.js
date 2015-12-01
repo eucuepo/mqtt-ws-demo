bf0a26ecd12137a064242dc8986828ba33ee0f22
Eugenio Cuevas Pozuelo <eucuepo@gmail.com>
2015-12-01 15:36:21 +0900

    working mqtt

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,8 +6,22 @@ angular.module('mqttDemo.controllers',[])
   $scope.message = 'Hello!';
 
 }])
-.controller('TestCtrl', ['$scope',function($scope) {
+.controller('TestCtrl', ['$scope','mqttClient',function($scope,mqttClient) {
 
+  var onConnectionLost = function(responseObject) {
+	console.log(responseObject);
+  }
+
+  var onMessageReceived = function(message) {
+	console.log(message);
+  }
+
+  var onConnect = function() {
+    console.log('connected!');  
+  }
+
+  mqttClient.init('development-jp-mqtt-8bfa32197598.internal.kii.com', 12470, 'hoc0CCowjuzV3wMSaLL6Lz8', '596cd936-x2gKbLiEnDx5EUCP76UzKnh','gjoFusjbpeCEFEVEowBnNekehieHiBlnqzVTUqJITBGjlhQNgaECKUToOvWtrZZT',onMessageReceived,onConnectionLost,onConnect);
   $scope.message = 'Hello!';
+
 
 }]);
--- public/js/services.js
+++ public/js/services.js
@@ -1,6 +1,24 @@
 'use strict';
 
 angular.module('mqttDemo.services',[])
-.service('MqttService', [function() {
+.service('mqttClient', [function mqttClientFactory() {
 
+  var client;
+
+  var subscribe = function(topic) {
+    client.subscribe(topic);
+  }
+
+  var init = function(host, port, clientID, username, password, messageHandler,disconnectHandler,onConnect) { 
+      client = new Paho.MQTT.Client(host, port, clientID);
+      // connect the client
+	  client.connect({onSuccess:onConnect, userName: username, password:password});
+	  client.onConnectionLost = messageHandler;
+      client.onMessageArrived = disconnectHandler;
+   }
+
+  return {
+    init: init,
+    subscribe: subscribe,
+  }
 }]);
